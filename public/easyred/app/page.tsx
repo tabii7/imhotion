d2f5760bc78cdb752d62816ffed7690746f28d9f
Server Sync <server@imhotion.easyred.com>
2025-09-08 18:00:06 +0200

    Server autosync: easy @ 2025-09-08T18:00:06+02:00

--- public/easyred/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import EasyredWebsite from "@/components/easyred-website"
-
-export default function Page() {
-  return <EasyredWebsite />
-}
--- public/easyred/next.config.mjs
+++ public/easyred/next.config.mjs
@@ -3,5 +3,7 @@ const nextConfig = {
   output: 'export',
   basePath: '/easyred',
   images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 export default nextConfig;
